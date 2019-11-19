60101a3afe3e9e44db64d824ef62fb09abe3f99c
Mikael Nystrom <deploymentbunny@outlook.com>
2019-11-19 14:49:50 +0100

    Update

---
+++ Get-Experts.ps1
@@ -0,0 +1,14 @@
+﻿# Get-Experts
+
+#Get Env:
+$RootFolder = $MyInvocation.MyCommand.Path | Split-Path -Parent
+$RootFolder = "D:\Repo\Get-Experts"
+#Get LData
+$XMLLDatafile = "$RootFolder\lConfig.XML"
+[XML]$XMLLData = Get-Content -Path $XMLLDatafile
+
+#Get Data
+$XMLDatafile = $XMLLData.Settings.XMLFile
+[XML]$XMLData = (New-Object System.Net.WebClient).DownloadString($XMLDatafile)
+
+$XMLData.Data.Experts.Expert
