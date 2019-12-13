0a8f51d74420fd04915c5919a58e5103fb2355f4
JordanTheItGuy <jordan.benzing@gmail.com>
2019-12-13 18:41:51 -0500

    Crap that actually works??
    Crap that actually kinda works???

--- Functions/Set-TSxLogging/Get-TSxLogging.psm1
+++ Functions/Set-TSxLogging/Get-TSxLogging.psm1
@@ -3,10 +3,10 @@ Function Write-TSxLog {
     param (
         [Parameter(Mandatory = $true)]
         [string]$Message,
-        [Parameter(HelpMessage = "Enter the log level",Mandatory=$false)]
+        [Parameter(HelpMessage = "Enter the log level", Mandatory = $false)]
         [ValidateSet(1, 2, 3)]
         [string]$LogLevel = 1,
-        [Parameter(HelpMessage = "This variable enables you to turn off writing the logged message to the screen it is on by default." ,Mandatory = $false)]
+        [Parameter(HelpMessage = "This variable enables you to turn off writing the logged message to the screen it is on by default." , Mandatory = $false)]
         [bool]$writetoscreen = $true,
         [Parameter(HelpMessage = "Enter the path to the log folder and it will change dynamically")]
         [string]$LogFoldderPath,
@@ -66,58 +66,74 @@ function set-TSxLogPath {
         [Parameter(Mandatory = $False, HelpMessage = "Enter the log file name", ParameterSetName = 'LogName')]
         [string]$LogFileName
     )
-    #region SetLogfileName
-    if ($LogFileName) {
-        if ($($LogFileName.Substring($($LogFileName.Length - 3)) -eq "log")) {
-            $LogFile = $LogFileName
-        }
-        else {
-            $LogFile = "$($LogFileName).log"
+    begin {
+        if (!($Global:TSxDefaultlogFile)) {
+            New-Variable -Name TSxDefaultLogFile -Value "$($ENV:TEMP)\TSxModule.log" -Scope Global
         }
     }
-    else {
-        if ($PSScriptRoot) {
-            $LogFile = "$($($script:MyInvocation.MyCommand.Name).Substring(0,$($script:MyInvocation.MyCommand.Name).Length-4)).log"
+    process {
+        #region SetLogfileName
+        if ($LogFileName) {
+            if ($($LogFileName.Substring($($LogFileName.Length - 3)) -eq "log")) {
+                $LogFile = $LogFileName
+            }
+            else {
+                $LogFile = "$($LogFileName).log"
+            }
         }
         else {
-            $LogFile = "TSXModule.log"
-            Write-Warning -Message "You are running inside ISE - all commands will be logged to: $($LogFile)"
+            if ($script:MyInvocation) {
+                $callstack = Get-PSCallStack
+                $LogFile = "$($($callstack[$callstack.Length-2].InvocationInfo.MyCommand.Name).Substring(0,$($callstack[$callstack.Length-2].InvocationInfo.MyCommand.Name).Length-4)).log"
+                $Global:CallStack = $callstack
+            }
+            else {
+                $LogFile = "TSXModule.log"
+                Write-Warning -Message "You are running inside ISE - all commands will be logged to: $($LogFile)"
+            }
         }
-    }
-    #endregion SetLogFileName
+        #endregion SetLogFileName
 
-    #Region SetLogFolderName
-    if ($LogFolderPath) {
-        $FullLogFile = "$($LogFolderPath)\$($logFile)"
-        Write-Debug -Message "This log has not been used before now initializing the path"
-        if($(Get-Variable -Name "TSx*" | Where-Object {$_.Value -like "*$($LogFile)"})){
-            $Global:TSxCurrentLogFile = $(Get-Variable -Name "TSX*$($LogFile)" | Where-Object {$_.Value -like "*$($LogFile)"}).Value
+        #Region SetLogFolderName
+        if ($LogFolderPath) {
+            $FullLogFile = "$($LogFolderPath)\$($logFile)"
+            Write-Debug -Message "This log has not been used before now initializing the path"
+            if ($(Get-Variable -Name "TSx*" | Where-Object { $_.Value -like "*$($LogFile)" })) {
+                $Global:TSxCurrentLogFile = $(Get-Variable -Name "TSX*$($LogFile)" | Where-Object { $_.Value -like "*$($LogFile)" }).Value
+            }
+            else {
+                New-Variable -Name "TSx$($LogFile)" -Scope Global -Value $FullLogFile
+                $Global:TSxCurrentLogFile = $(Get-Variable -Name "TSX*$($LogFile)" | Where-Object { $_.Value -like "*$($LogFile)" }).Value
+            }
         }
-        else{
-            New-Variable -Name "TSx$($LogFile)" -Scope Global -Value $FullLogFile
-            $Global:TSxCurrentLogFile = $(Get-Variable -Name "TSX*$($LogFile)" | Where-Object {$_.Value -like "*$($LogFile)"}).Value
+        elseif ($PSScriptRoot) {
+            $Name = $CallStack[-2].InvocationInfo.MyCommand.Name
+            $Source = $CallStack[-2].InvocationInfo.MyCommand.Source
+            $LogFolderPath = $Source.Replace($Name, "")
+            $Global:TSxCurrentLogFile = "$($LogFolderPath)$($Logfile)"
         }
-    }
-    else {
-        if($(Get-Variable -Name "TSx*" | Where-Object {$_.Value -like "*$($LogFile)"})){
-            $Global:TSxCurrentLogFile = $(Get-Variable -Name "TSX*$($LogFile)" | Where-Object {$_.Value -like "*$($LogFile)"}).Value
+        else {
+            if ($(Get-Variable -Name "TSx*" | Where-Object { $_.Value -like "*$($LogFile)" })) {
+                $Global:TSxCurrentLogFile = $(Get-Variable -Name "TSX*$($LogFile)" | Where-Object { $_.Value -like "*$($LogFile)" }).Value
+            }
+            else {
+                Write-Warning -Message "You specified a log that doesn't exist, or hasn't been declared - all commands will be logged to: $($Global:TSxDefaultlogFile)"
+                $Global:TSxCurrentLogFile = $Global:TSxDefaultLogFile
+            }
         }
-        else{
-            $LogFile = "$($ENV:TEMP)\TSxModule.log"
-            Write-Warning -Message "You specified a log that doesn't exist, or hasn't been declared - all commands will be logged to: $($LogFile)"
-            $Global:TSxCurrentLogFile = $LogFile
+        #endregion SetLogFolderName
+        try {
+            #Confirm the provided destination for logging exists if it doesn't then create it.
+            if (!(Test-Path $Global:TSxCurrentLogFile)) {
+                ## Create the log file destination if it doesn't exist.
+                New-Item $Global:TSxCurrentLogFile -Type File | Out-Null
+            }
         }
-    }
-    #endregion SetLogFolderName
-    try {
-        #Confirm the provided destination for logging exists if it doesn't then create it.
-        if (!(Test-Path $Global:TSxCurrentLogFile)) {
-            ## Create the log file destination if it doesn't exist.
-            New-Item $Global:TSxCurrentLogFile -Type File | Out-Null
+        catch {
+            #In event of an error write an exception
+            Write-Error $_.Exception.Message
         }
-    }
-    catch {
-        #In event of an error write an exception
-        Write-Error $_.Exception.Message
     }
 }
+
+Write-TSxLog -Message "Still works using F8 command from De-Bug in VSCode"
